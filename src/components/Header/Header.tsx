e8d597b4d6f55655f2447f9e191683fce0150c7a
Eugene Cross <eugene.cross@gmail.com>
2023-04-11 08:54:45 +0100

    feat: Add RSK logo and resize

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import logo from '../../assets/images/logo.png';
+import pledgeball_logo from '../../assets/images/Pledgeball_logo.png';
+import rsk_logo from '../../assets/images/RSK_logo.png';
 
 import './Header.scss'
 export default function Header() {
   return (
      <header className="header"> 
-        <img src={logo} className="logo" alt="logo" />
+        <img src={pledgeball_logo} className="pledgeball-logo" alt="logo" />
+        <img src={rsk_logo} className="rsk-logo" alt="logo" />
       </header> 
   )
 }
